9aa8f38f4e295bb1e471d187fe9797e7df77302d
Stephen Bussey <sb8244@cs.ship.edu>
2021-07-16 00:07:39 -0400

    Cleanup some unused code

--- lib/sql_builder/application.ex
+++ lib/sql_builder/application.ex
@@ -1,32 +1,20 @@
 defmodule SqlBuilder.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
   @moduledoc false
 
   use Application
 
   def start(_type, _args) do
     children = [
-      # Start the Ecto repository
       SqlBuilder.Repo,
-      # Start the Telemetry supervisor
       SqlBuilderWeb.Telemetry,
-      # Start the PubSub system
       {Phoenix.PubSub, name: SqlBuilder.PubSub},
-      # Start the Endpoint (http/https)
       SqlBuilderWeb.Endpoint
-      # Start a worker by calling: SqlBuilder.Worker.start_link(arg)
-      # {SqlBuilder.Worker, arg}
     ]
 
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: SqlBuilder.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
   def config_change(changed, _new, removed) do
     SqlBuilderWeb.Endpoint.config_change(changed, removed)
     :ok
--- lib/sql_builder_web/endpoint.ex
+++ lib/sql_builder_web/endpoint.ex
@@ -10,10 +10,6 @@ defmodule SqlBuilderWeb.Endpoint do
     signing_salt: "iV5Xye4o"
   ]
 
-  socket "/socket", SqlBuilderWeb.UserSocket,
-    websocket: true,
-    longpoll: false
-
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
 
   # Serve at "/" the static files from "priv/static" directory.
